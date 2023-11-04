ebfaa0b068687e6d3b9ef6faaf18ae989cc1815b
ValenGaymer <valenbustamanteq@gmail.com>
2023-11-04 08:51:37 -0500

    e

--- src/client.py
+++ src/client.py
@@ -12,7 +12,7 @@ num_rows = 3
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((HOST, PORT))
 
-    while True:
+    for i in range(10):
         data = {
             'Velocidad del viento km/h': [random.randint(100, 150) for _ in range(num_rows)],
             'Precipitación %': [random.randint(0, 50) for _ in range(num_rows)],
@@ -28,14 +28,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.send(data_to_send)
         print(df)
         print("DataFrame enviado al servidor.")
-
-        # Espera la respuesta del servidor
-        response_size_bytes = client_socket.recv(4)
-        response_size = int.from_bytes(response_size_bytes, byteorder='big')
-        response_data = client_socket.recv(response_size)
-        response_df = pickle.loads(response_data)
-
-        print("DataFrame final recibido del servidor:")
-        print(response_df)
-
         time.sleep(1)
+
+    response_size_bytes = client_socket.recv(4)
+    response_size = int.from_bytes(response_size_bytes, byteorder='big')
+    response_data = client_socket.recv(response_size)
+    response_df = pickle.loads(response_data)
+    print("DataFrame final recibido del servidor:")
+    print(response_df)
