9783b9dffd6bbe5b63b07398dfb7bbcf5c70edd6
ValenGaymer <valenbustamanteq@gmail.com>
2023-11-08 20:42:47 -0500

    deede

--- src/server.py
+++ src/server.py
@@ -1,26 +1,56 @@
+import threading
+import pandas as pd
 import socket
 import pickle
 
-HOST = '10.20.2.38'
-PORT = 8081
+HOST = '192.168.101.82'
+PORT = 8253
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind((HOST, PORT))
-        server_socket.listen()
-        print(f"Esperando {HOST}:{PORT}")
-        conn, addr = server_socket.accept()
-        with conn:
-            print(f"Nueva conexión desde {addr}")
-            while True:
-                data_size_bytes = conn.recv(4)
-                if not data_size_bytes:
-                    break
-                
-                data_size = int.from_bytes(data_size_bytes, byteorder='big')
-                data = conn.recv(data_size)
-                if not data:
-                    break   
-                df = pickle.loads(data)
-                print("DataFrame recibido")
-                print(df)
+client_dataframes = {}
+sensores_lista = []
+lock = threading.Lock()
+
+def handle_client(conn):
+    try:
+        client_df = pd.DataFrame()  # Crear un DataFrame para el cliente actual
+        while len(client_df) < 30:  # Esperar a recibir 10 dataframes
+
+            data_size_bytes = conn.recv(4)
+            if not data_size_bytes:
+                break
+            
+            data_size = int.from_bytes(data_size_bytes, byteorder='big')
+            data = b""
+            while len(data) < data_size:
+                more_data = conn.recv(data_size - len(data))
+                if not more_data:
+                    raise Exception("Recibido menos datos de lo esperado")
+                data += more_data
+                print(data)
+            df = pickle.loads(data)
+            
+            client_df = pd.concat([client_df, df], axis=0, ignore_index=True)
+            print(f'client_Df:     {client_df}')
+        
+
+        sensores_lista.append("sensor")
+
+        while len(sensores_lista) < 3:
+            print(f'TAMAÑO LISTAA {len(sensores_lista)}\n')
+            pass
+
+        with lock:
+            client_dataframes[threading.current_thread().ident] = client_df
+            print(f'wlock:    {client_dataframes[threading.current_thread().ident]}')
+            final_dataframe = pd.concat(client_dataframes, axis=1)
+
+        print(final_dataframe)
+        conn.send(len(pickle.dumps(final_dataframe)).to_bytes(4, byteorder='big'))
+        conn.send(pickle.dumps(final_dataframe))
+    except Exception as e:
+        print(f"Error al manejar cliente: {e}")
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
+print(f"Esperando {HOST}:{PORT}")
